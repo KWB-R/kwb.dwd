f33863b19d19ffd8238f1a604267437544e04546
hsonne <hauke.sonnenberg@gmx.de>
2019-02-24 15:40:15 +0100

    Add description and example to get_radolan_urls()

--- R/dwd_radolan.R
+++ R/dwd_radolan.R
@@ -1,4 +1,11 @@
-#' Get URLs to Available Radolan Files on DWD Server
+#' Get URLs to Available Radolan Files
+#'
+#' Get URLs to available radolan files on the DWD FTP server below this base
+#' address: \url{ftp://ftp-cdc.dwd.de/pub/CDC/grids_germany}. The user can
+#' choose between daily records and hourly records that are located at different
+#' paths on the server. The paths to the files are not read from the FTP server
+#' but generated, given the knowledge of where the files should reside and how
+#' they are expected to be named.
 #'
 #' @param start_daily month string (yyyy-mm) of first daily record.
 #'   Default: "2006-10"
@@ -8,13 +15,32 @@
 #'   current month.
 #' @param end_hourly month string (yyyy-mm) of last hourly records. Defaults to
 #'   \code{end_daily}.
+#'
 #' @importFrom magrittr %>%
 #' @importFrom kwb.utils resolve
 #' @importFrom lubridate rollback
 #' @importFrom stringr str_replace
 #' @importFrom fs dir_create
+#'
 #' @return list with "daily_historical_urls" and "hourly_historical_urls"
+#'
 #' @export
+#'
+#' @examples
+#' Get all expected URLs
+#' urls <- kwb.dwd::get_radolan_urls()
+#'
+#' # Show the first URLs of each category (daily, hourly)
+#' head(urls$daily_historical_urls)
+#' head(urls$hourly_historical_urls)
+#'
+#' # Set the start and end months
+#' urls <- kwb.dwd::get_radolan_urls(
+#'   start_daily = "2017-01", end_daily = "2017-03"
+#' )
+#'
+#' urls$daily_historical_urls
+#'
 get_radolan_urls <- function(
   start_daily = "2006-10",
   start_hourly = "2005-06",
