7f6889897dd75065626975cd97b38167b5a48863
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-05-20 21:21:46 +0200

    Add and use get_radolan_projection_string()

--- R/crop_radolan_radial.R
+++ R/crop_radolan_radial.R
@@ -16,25 +16,10 @@ crop_radolan_radial <- function(radolan, longitude, latitude, radius = 10)
   # Transform lat/lon input to st object
   location <- sf::st_as_sf(coord, coords = c("lon", "lat"), crs = 4326)
 
-  # Compose radolan projection string
-  projection_string <- to_projection_string(
-    parameters = list(
-      proj = "stere",
-      lat_0 = 90,
-      lat_ts = 90,
-      lon_0 = 10,
-      k = 0.93301270189,
-      x_0 = 0,
-      y_0 = 0,
-      a = 6370040,
-      b = 6370040,
-      to_meter = 1000
-    ),
-    switches = "no_defs"
-  )
-
-  # Define coordinate reference system
-  reference_system <- raster::crs(sp::CRS(projection_string), asText = TRUE)
+  # Define coordinate reference system for Radolan data
+  reference_system <- raster::crs(asText = TRUE, sp::CRS(
+    get_radolan_projection_string()
+  ))
 
   # Apply radolan projection to location
   location_crs <- sf::st_transform(x = location, crs = reference_system)
@@ -52,11 +37,4 @@ crop_radolan_radial <- function(radolan, longitude, latitude, radius = 10)
     SAMPLE_DATE = lubridate::ymd(substr(names(x), 2, 7)),
     rain_mean = as.numeric(x)
   )
-}
-
-# to_projection_string ---------------------------------------------------------
-to_projection_string <- function(parameters, switches)
-{
-  assignments <- paste0(names(parameters), "=", as.character(parameters))
-  paste0("+", c(assignments, switches), collapse = " ")
 }
---
+++ R/get_radolan_projection_string.R
@@ -0,0 +1,26 @@
+# Compose radolan projection string
+get_radolan_projection_string <- function()
+{
+  to_projection_string(
+    parameters = list(
+      proj = "stere",
+      lat_0 = 90,
+      lat_ts = 90,
+      lon_0 = 10,
+      k = 0.93301270189,
+      x_0 = 0,
+      y_0 = 0,
+      a = 6370040,
+      b = 6370040,
+      to_meter = 1000
+    ),
+    switches = "no_defs"
+  )
+}
+
+# to_projection_string ---------------------------------------------------------
+to_projection_string <- function(parameters, switches)
+{
+  assignments <- paste0(names(parameters), "=", as.character(parameters))
+  paste0("+", c(assignments, switches), collapse = " ")
+}
