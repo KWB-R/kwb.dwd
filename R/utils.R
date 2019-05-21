f14a6182c622cd232d9f54d811c69b85d208332d
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-05-21 15:54:28 +0200

    Add extract_radolan_zip_files() and helpers
    New helper functions:
    - get_date_time_from_bin_filename()
    - list_files_in_zip_files()

--- R/utils.R
+++ R/utils.R
@@ -12,10 +12,43 @@ date_in_bathing_season <- function(x)
   months >= 5 & months < 10
 }
 
+# get_date_time_from_bin_filename ----------------------------------------------
+get_date_time_from_bin_filename <- function(x)
+{
+  format <- "raa01-sf_10000-%y%m%d%H%M-dwd---bin"
+  times <- as.POSIXct(basename(x), format = format, tz = "UTC")
+
+  is_na <- is.na(times)
+
+  if (any(is_na)) {
+    warning(
+      "For ", sum(is_na), " files, the date and time could not be determined: ",
+      kwb.utils::stringList(head(x[is_na])), call. = FALSE
+    )
+  }
+
+  times
+}
+
 # get_element_or_stop ----------------------------------------------------------
 get_element_or_stop <- function(x, element, name = deparse(substitute(element)))
 {
   x[safe_element(element, names(x), name)]
+}
+
+# list_files_in_zip_files ------------------------------------------------------
+list_files_in_zip_files <- function(zip_files, dbg = TRUE)
+{
+  do.call(rbind, lapply(zip_files, function(x) {
+    kwb.utils::catAndRun(
+      messageText = paste("Getting names of files in", x),
+      dbg = dbg,
+      expr = kwb.utils::noFactorDataFrame(
+        zip_file = basename(x),
+        file = utils::untar(x, list = TRUE)
+      )
+    )
+  }))
 }
 
 # month_sequence ---------------------------------------------------------------
