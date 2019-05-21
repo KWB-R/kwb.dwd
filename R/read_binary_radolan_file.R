761afb08f382b1590952c9e6c6d4c0250a923101
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-05-21 16:23:48 +0200

    Add read_binary_radolan_file()
    This function combines the steps
    - reading raw values into a vector
    - create raster object from vector
    - set projection and extent (by default)

---
+++ R/read_binary_radolan_file.R
@@ -0,0 +1,34 @@
+# read_binary_radolan_file -----------------------------------------------------
+
+#' Read Binary Radolan File
+#'
+#' @param path path to binary Radolan file
+#' @param set_projection_and_extent if \code{TRUE} (default), the projection
+#'   and extent of the raster object are set to the Radolan specific values,
+#'   returned by \code{kwb.dwd:::get_radolan_projection_string()} and
+#'   \code{get_radolan_extension()}, respectively.
+#'
+read_binary_radolan_file <- function(path, set_projection_and_extent = TRUE)
+{
+  # Read raw data values
+  rbi <- read_binary_radolan_file_raw(path)
+
+  # Convert raw data values to raster
+  rb <- radolan_raw_to_raster(rbi)
+
+  # Set projection and extent
+  if (set_projection_and_extent) {
+
+    raster::projection(rb) <- sp::CRS(get_radolan_projection_string())
+    raster::extent(rb) <- raster::extent(get_radolan_extension())
+  }
+
+  # Return the raster object
+  rb
+}
+
+# get_radolan_extension --------------------------------------------------------
+get_radolan_extension <- function()
+{
+  c(-523.4622, 376.5378, -4658.645, -3758.645)
+}
