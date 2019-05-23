87ef7f35c3b7dbad2312bb2fca415bb4e44e932a
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-05-23 17:47:04 +0200

    Save all ftp paths to a file

--- inst/extdata/list_all_dwd_urls.R
+++ inst/extdata/list_all_dwd_urls.R
@@ -6,11 +6,33 @@ if (FALSE)
   #urls <- kwb.dwd::list_url(base_url, recursive = TRUE, n_attempts = 1)
   #failed <- attr(urls, "failed")
 
-  result_urls <- kwb.dwd:::list_ftp_files_recursively(base_url)
+  result_urls <- kwb.dwd:::list_ftp_files_recursively(
+    base_url, n_trials = 5, sleep_between_trials = 0.1, sleep_between_runs = 1,
+    max_depth = 2
+  )
+
+  subdir_matrix <- kwb.file:::to_subdir_matrix(result_urls)
+
+  View(subdir_matrix)
+
+  # Trying to read 6 failing URLs again...
+  # ftp://ftp-cdc.dwd.de/pub/CDC/derived_germany/techn/monthly/cooling_degreehours/cdh_13/historical/: ok.
+  # ftp://ftp-cdc.dwd.de/pub/CDC/derived_germany/techn/monthly/cooling_degreehours/cdh_16/historical/: ok.
+  # ftp://ftp-cdc.dwd.de/pub/CDC/derived_germany/techn/monthly/cooling_degreehours/cdh_16/recent/: ok.
+  # ftp://ftp-cdc.dwd.de/pub/CDC/derived_germany/techn/monthly/cooling_degreehours/cdh_18/historical/: ok.
+  # ftp://ftp-cdc.dwd.de/pub/CDC/derived_germany/techn/monthly/cooling_degreehours/cdh_18/recent/: ok.
+
+  url <- "ftp://ftp-cdc.dwd.de/pub/CDC/derived_germany/techn/monthly/cooling_degreehours/cdh_13/historical/"
+  kwb.dwd::list_url(url)
+  RCurl::getURL(url)
 
   length(result_urls)
 
-  writeLines(result_urls)
+  file <- format(Sys.time(), format = "inst/extdata/urls_dwd_%Y%m%d_%H%M.txt")
+
+  getwd()
+
+  writeLines(result_urls, file)
 
   urls_list
 
