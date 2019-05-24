b6b5ebe29a790cd97a521b863b0badf136a09bb8
hsonne <hauke.sonnenberg@gmx.de>
2019-05-24 05:07:31 +0200

    Try new version of list_ftp_files_recursively()

--- R/list_ftp_files_recursively.R
+++ R/list_ftp_files_recursively.R
@@ -46,3 +46,51 @@ merge_url_list <- function(url_list)
     failed = unlist(lapply(url_list, attr, "failed"))
   )
 }
+
+# list_ftp_files_recursively_2 ---------------------------------------------------
+list_ftp_files_recursively_2 <- function(
+  url, n_trials = 3, sleep_between_trials = 5, sleep_between_runs = 10, ...
+)
+{
+  result <- list()
+  on.exit(return(sort(unlist(result))))
+
+  failed <- url
+
+  while (length(failed)) {
+
+    url_list <- lapply(
+      failed,
+      list_url,
+      recursive = TRUE,
+      n_trials = n_trials,
+      sleep_time = sleep_between_trials,
+      ...
+    )
+
+    full_urls <- merge_url_list_2(url_list, failed)
+
+    result[[length(result) + 1]] <- structure(full_urls, failed = NULL)
+
+    failed <- attr(full_urls, "failed")
+
+    if (length(failed)) {
+      message("Trying to read ", length(failed), " failing URLs again...")
+      Sys.sleep(sleep_between_runs)
+    }
+  }
+}
+
+# merge_url_list_2 ---------------------------------------------------------------
+merge_url_list_2 <- function(url_list, base_urls)
+{
+  merged <- if (length(url_list)) {
+    unlist(lapply(seq_along(url_list), function(i) {
+      paste0(assert_trailing_slash(base_urls[i]), url_list[[i]])
+    }))
+  } else {
+    character()
+  }
+
+  structure(merged, failed = unlist(lapply(url_list, attr, "failed")))
+}
