6b25144b090f5e40b9c92f4e7b6fc795c63e43a8
hsonne <hauke.sonnenberg@gmx.de>
2019-06-22 22:26:05 +0200

    Provide full file info in dataset "dwd_files"
    - Remove dataset "opendata_files.rda" (dwd_files) now
      refers to the URLs below
      ftp://opendata.dwd.de/climate_environment/CDC/

--- R/datasets.R
+++ R/datasets.R
@@ -1,30 +1,16 @@
-#' Files Available on DWD's FTP Server (deprecated)
-#'
-#' URLs to files that are/were available for download at
-#' \url{ftp://ftp-cdc.dwd.de/pub/CDC}. The file list was last updated on
-#' 2019-05-31. According to DWD, this server closes on 2019-06-01. Instead,
-#' files can be downloaded from
-#' \url{ftp://opendata.dwd.de/climate_environment/CDC/},
-#' see \code{\link{opendata_files}}.
-#'
-#' @format A data frame with 355311 rows and 2 variables:
-#' \describe{
-#'   \item{path}{URL of the path to the file on the FTP server}
-#'   \item{file}{file name}
-#' }
-#' @source \url{ftp://ftp-cdc.dwd.de/pub/CDC/Announce_log_CDC_ftp.txt}
-"dwd_files"
-
 #' Files Available on DWD's FTP Server
 #'
-#' URLs to files that are available for download at
-#' \url{ftp://opendata.dwd.de/climate_environment/CDC/}. The file list was last
-#' updated on 2019-06-01.
+#' Information on files that are available for download at
+#' \url{ftp://opendata.dwd.de/climate_environment/CDC/}. The information were
+#' last updated on 2019-06-22.
 #'
-#' @format A data frame with 436687 rows and 2 variables:
+#' @format A data frame with 438548 observations of 5 variables:
 #' \describe{
-#'   \item{path}{URL of the path to the file on the FTP server}
-#'   \item{file}{file name}
+#'   \item{url}{URL to the file on the FTP server}
+#'   \item{type}{either "file" or "directory"}
+#'   \item{size}{file size in bytes}
+#'   \item{permissions}{string indicating file permissions}
+#'   \item{modification_time}{time of last modification of the file}
 #' }
 #' @source \url{ftp://opendata.dwd.de/climate_environment/CDC/Announce_log_CDC_ftp.txt}
-"opendata_files"
+"dwd_files"
