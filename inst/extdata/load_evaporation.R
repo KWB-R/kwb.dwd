ffaf21a47581494f8cb499b4d59005bcd1352e22
amatzi <40765874+amatzi@users.noreply.github.com>
2020-04-15 15:20:19 +0200

    calculate stats for Berlin subset
    I tried to add two functions and two corresponding lines to main script
    
    I also tried to submit the Berlin coordinates in a DWD-format matrix, but somehow managed to open another branch....and basically have no idea how to link to the uploaded data....so I left my local address as placeholder in Line 82...please check

--- inst/extdata/load_evaporation.R
+++ inst/extdata/load_evaporation.R
@@ -8,7 +8,7 @@
 # MAIN -------------------------------------------------------------------------
 if (FALSE)
 {
-  # Base URL to evaporation files on DWD server
+  # Base URL to potential evaporation files on DWD server
   base_url <- kwb.dwd:::ftp_path_cdc("grids_germany/monthly/evapo_p")
   
   # List data files
@@ -32,6 +32,15 @@ if (FALSE)
   head(file_info)
   
   str(evaporation_matrices[[1]])
+  
+  # Get Berlin matrix, same size as DWD evpo matrix (Berlin grid cells set to 1, rest of cells = NA)
+    Berlin_DWD_mask <- Berlin_DWD_mask()
+  
+  # calculate monthly stats for Berlin
+    Berlin_evap_monthly <- evaporation_stats(evaporation_matrices = evaporation_matrices,
+                                             file_info = file_info,
+                                             geo_mask = Berlin_DWD_mask)
+  
 }
 
 # read_evaporation_matrix_from_url ---------------------------------------------
@@ -62,3 +71,51 @@ read_evaporation_matrix_from_url <- function(url)
     month = extract_date_part(2L)
   )
 }
+
+# get geographical "stamp" for Berlin area ---------------------------------
+Berlin_DWD_mask <- function()
+{
+  #DWD matrix filled with NA
+  DWD_dim <- matrix(NA, nrow = 866, ncol = 654)
+  
+  #get Berlin coordinates...I did not find out how to link to the file I uploaded!!!
+  Berlin_coordinates <- foreign::read.dbf('C:/Aendu_lokal/ABIMO_Paper/Daten/Karten/Hilfsgrid_DWD/Grid_Berlin_DWD.dbf')[,6:7]
+  
+  #set Berlin cells to 1 
+  Berlin_matrix <- DWD_dim
+  
+  for (i in 1:length(grid_Berlin_DWD$row)) {
+      Berlin_matrix[grid_Berlin_DWD$row[i], grid_Berlin_DWD$col[i]] <- 1
+  }
+  
+  Berlin_matrix
+  
+}
+
+# calculate stats of potential evaporation for geographical subset -------------------------------------------
+evaporation_stats <- function(evaporation_matrices,
+                              file_info,
+                              geo_mask)
+{
+  pot_evap_stat <- file_info
+  
+
+  for (i in 1:length(evaporation_matrices)) {
+    
+    #keep only Berlin grid cells  
+    Berlin_values <- evaporation_matrices[[i]] * geo_mask
+    
+    #correct unit to mm
+    Berlin_values <- Berlin_values / 10
+    
+    pot_evap_stat$mean[i] <- mean(Berlin_values, na.rm = TRUE)
+    pot_evap_stat$sd[i] <- sd(Berlin_values, na.rm = TRUE)
+    pot_evap_stat$min[i] <- min(Berlin_values, na.rm = TRUE)
+    pot_evap_stat$max[i] <- max(Berlin_values, na.rm = TRUE)
+    
+  }
+  
+  pot_evap_stat
+  
+}
+
