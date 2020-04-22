6db19744615176b6b9fd64b52b8884611e05140e
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2020-04-22 21:56:44 +0200

    Call functions with package:: to satisfy CHECK

--- R/potential_evaporation.R
+++ R/potential_evaporation.R
@@ -20,7 +20,7 @@ read_evaporation_matrix_from_url <- function(url)
   extract_date_part <- function(i) as.integer(year_month[[i]])
 
   structure(
-    as.matrix(read.table(text = text[-(1:6)])),
+    as.matrix(utils::read.table(text = text[-(1:6)])),
     header = text[1:6],
     year = extract_date_part(1L),
     month = extract_date_part(2L)
@@ -34,7 +34,9 @@ get_berlin_dwd_mask <- function()
   berlin_matrix <- matrix(NA, nrow = 866, ncol = 654)
 
   #get Berlin coordinates
-  berlin_coordinates <- read.csv(system.file("extdata/berlin_coordinates.csv", package = "kwb.dwd"))
+  berlin_coordinates <- utils::read.csv(system.file(
+    "extdata/berlin_coordinates.csv", package = "kwb.dwd"
+  ))
 
   #set Berlin cells to 1
 
@@ -43,7 +45,6 @@ get_berlin_dwd_mask <- function()
   }
 
   berlin_matrix
-
 }
 
 # calculate stats of potential evaporation for geographical subset -------------
@@ -61,10 +62,9 @@ evaporation_stats <- function(evaporation_matrices, file_info, geo_mask)
 
     get_stats <- function(fun) fun(berlin_values, na.rm = TRUE)
     pot_evap_stat$mean[i] <- get_stats(mean)
-    pot_evap_stat$sd[i] <- get_stats(sd)
+    pot_evap_stat$sd[i] <- get_stats(stats::sd)
     pot_evap_stat$min[i] <- get_stats(min)
     pot_evap_stat$max[i] <- get_stats(max)
-
   }
 
   pot_evap_stat
