640a466ddb50fd8a60bcc0d50d456c85a2aebdb2
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2020-05-31 19:15:07 +0200

    Further refactor and simplify list_url_()

---
+++ R/list_contents.R
@@ -0,0 +1,132 @@
+# list_contents ----------------------------------------------------------------
+list_contents <- function(x, full_info, ...)
+{
+  # Get a response from the FTP server
+  response <- try_to_get_url(x, ...)
+  #response <- try_to_get_url(x)
+
+  # Convert response string to data frame
+  info <- response_to_data_frame(response, full_info)
+
+  # Return if info is empty. A response of NULL indicates that an error occurred
+  # when reading from the URL. In this case, set the attribute "failed" to the
+  # URL that failed to be accessed.
+  structure(info, failed = if (is.null(response)) x)
+}
+
+# response_to_data_frame -------------------------------------------------------
+response_to_data_frame <- function(response, full_info = FALSE)
+{
+  template <- empty_file_info(full_info)
+
+  # Response is NULL or equal to an empty string?
+  if (is.null(response) || grepl("^\\s*$", response)) {
+    return(template)
+  }
+
+  info <- response_to_data_frame_raw(response)
+
+  get_info <- function(x) kwb.utils::selectColumns(info, x)
+
+  # Required column: is_directory
+  info$is_directory <- grepl("^d", get_info("permissions"))
+
+  if (! full_info) {
+    return(get_info(c("file", "is_directory")))
+  }
+
+  #info$type <- ifelse(info$is_directory, "directory", "file")
+
+  # Replace columns "year_or_time", "month", "day" with "modification_time".
+  # Put the most important columns first.
+  main_columns_first(simplify_time_info(info))
+}
+
+# empty_file_info --------------------------------------------------------------
+empty_file_info <- function(full_info = TRUE)
+{
+  full_record <- kwb.utils::noFactorDataFrame(
+    file = character(),
+    is_directory = logical(),
+    #type = character(),
+    size = numeric(),
+    permissions = character(),
+    modification_time = character(),
+    user = character(),
+    group = character()
+  )
+
+  if (full_info) {
+
+    full_record
+
+  } else {
+
+    full_record[, 1:2]
+  }
+}
+
+# response_to_data_frame_raw ---------------------------------------------------
+response_to_data_frame_raw <- function(response)
+{
+  # Split response at new line character into single rows
+  rows <- split_into_lines(response)
+
+  # Example row
+  # "-rw-r--r--    1 9261     15101        9132 Mar 27 15:05"
+
+  # Keep only the left-hand parts, representing eight columns (separated by one
+  # or more spaces), from each row
+  text <- kwb.utils::extractSubstring("^((([^ ]+) +){8})", rows, index = 1L)
+
+  # Convert the text rows into a data frame of file properties (except name)
+  info <- utils::read.table(
+    text = text, stringsAsFactors = FALSE, col.names = c(
+      "permissions", "links", "user", "group", "size", "month", "day",
+      "year_or_time"
+    )
+  )
+
+  # Set the filenames (right parts of the rows) in the returned info data frame
+  kwb.utils::setColumns(dbg = FALSE, info, file = mapply(
+    right, rows, nchar(rows) - nchar(text), USE.NAMES = FALSE
+  ))
+}
+
+# simplify_time_info -----------------------------------------------------------
+simplify_time_info <- function(info)
+{
+  # Add column "modification_time"
+  info$modification_time <- columns_to_timestamp(info)
+
+  # Remove the columns that were used to create the timestamps
+  kwb.utils::removeColumns(info, c("year_or_time", "month", "day"))
+}
+
+# columns_to_timestamp ---------------------------------------------------------
+columns_to_timestamp <- function(info)
+{
+  pull <- function(x) kwb.utils::selectColumns(info, x)
+
+  years_or_times <- pull("year_or_time")
+
+  is_year <- ! grepl(":", years_or_times)
+
+  # Compose a vector of timestamps. Use the current year in case of missing
+  # years, and use midnight in case of missing times
+  sprintf(
+    "%04d-%02d-%02d %s",
+    as.integer(ifelse(is_year, years_or_times, format(Sys.Date(), "%Y"))),
+    sapply(pull("month"), kwb.utils::selectElements, x = month_numbers()),
+    as.integer(pull("day")),
+    ifelse(is_year, "00:00", years_or_times)
+  )
+}
+
+# main_columns_first -----------------------------------------------------------
+main_columns_first <- function(df)
+{
+  columns <- intersect(names(empty_file_info(full_info = TRUE)), names(df))
+
+  kwb.utils::moveColumnsToFront(df, columns)
+}
--- R/list_url_.R
+++ R/list_url_.R
@@ -5,8 +5,8 @@
 # @param depth for start depth when \code{recursive = TRUE}
 # @param curl RCurl handle passed to \code{kwb.dwd:::try_to_get_url}
 list_url_ <- function(
-  url, recursive = TRUE, max_depth = 1, full_info = FALSE, ..., curl, depth = 0,
-  random_failures = TRUE
+  url, recursive = TRUE, max_depth = 1, full_info = FALSE, ..., depth = 0,
+  prob_mutate = 0.1
 )
 {
   if (FALSE) {
@@ -19,81 +19,66 @@ list_url_ <- function(
   # Check URL and append slash if necessary
   url <- assert_url(url)
 
-  # Shall a failure be simulated?
-  mutate <- random_failures && sample(c(TRUE, FALSE), 1, prob = c(0.1, 0.9))
-
-  # Provide URL so to for (eventually randomly modified)
-  url_to_go <- paste0(url, if (mutate) "blabla")
-
-  # Get a response from the FTP server
-  response <- try_to_get_url(url_to_go, curl = curl, ...)
-  #response <- try_to_get_url(url_to_go)
-
-  # Convert response string to data frame
-  info <- response_to_data_frame(response, full_info)
-
-  # Return if info is empty. A response of NULL indicates that an error occurred
-  # when reading from the URL. In this case, set the attribute "failed" to the
-  # URL that failed to be accessed.
-  if (is_empty(info)) {
-    return(structure(info, failed = if (is.null(response)) url))
-  }
+  # Call the domain specific function list_contents(). The function is expected
+  # to set the attribute "failed" to the given URL in case that the URL failed
+  # to be accessed.
+  info <- list_contents(mutate_or_not(url, prob_mutate), full_info, ...)
+  #info <- list_contents(mutate_or_not(url, prob_mutate), full_info)
 
   # Helper function to get an info column
-  info_column <- function(x) kwb.utils::selectColumns(info, x)
-
-  # Extract the file names
-  files <- info_column("file")
+  get_info <- function(x) kwb.utils::selectColumns(info, x)
 
   # Which files represent directories?
-  is_directory <- info_column("is_directory")
+  is_directory <- get_info("is_directory")
 
   # Return the file list if no recursive listing is requested or if we are
-  # already at maximum depth or if there are no directories
+  # already at maximum depth or if there are no directories. The function is
+  # also returned from if info is empty (! any(is_directory) is TRUE).
   if (! recursive || at_max_depth(depth, max_depth) || ! any(is_directory)) {
 
     # Indicate directories with trailing slash
-    return(kwb.utils::setColumns(
-      info, file = indicate_directories(files, is_directory)
-    ))
+    info$file <- indicate_directories(get_info("file"), is_directory)
+
+    return(info)
   }
 
-  # If we arrive here, a recursive listing is requested
-
-  # Number of directories
+  # If we arrive here, a recursive listing is requested. Number of directories
   n_directories <- sum(is_directory)
 
-  # If there are directories, list them by calling this function recursively
-  files_in_dirs <- if (n_directories > 0L) {
+  # If there are directories (always if we arrive here!), list them by calling
+  # this function recursively
 
-    # Helper function to show the progress
-    show_progress <- function(i) cat(sprintf(
-      "%s%d/%d: ", repeated("  ", depth), i, n_directories
-    ))
+  #files_in_dirs <- if (n_directories > 0L) {
 
-    # URLs representing directories
-    directories <- files[is_directory]
+  # Helper function to show the progress
+  show_progress <- function(i) cat(sprintf(
+    "%s%d/%d: ", repeated("  ", depth), i, n_directories
+  ))
 
-    # List all directories
-    url_lists <- lapply(seq_len(n_directories), function(i) {
+  # URLs representing directories
+  directories <- get_info("file")[is_directory]
 
-      show_progress(i)
+  # List all directories
+  url_lists <- lapply(seq_len(n_directories), function(i) {
 
-      # Recursive call of this function
-      list_url_(
-        url = paste0(url, directories[i]),
-        recursive = recursive,
-        max_depth = max_depth,
-        full_info = full_info,
-        ...,
-        curl = curl,
-        depth = depth + 1
-      )
-    })
+    #i <- 1L
+    show_progress(i)
 
-    merge_url_lists(url_lists, directories, full_info)
+    # Recursive call of this function
+    list_url_(
+      url = paste0(url, directories[i]),
+      recursive = recursive,
+      max_depth = max_depth,
+      full_info = full_info,
+      ...,
+      depth = depth + 1,
+      prob_mutate = prob_mutate
+    )
+  })
 
-  } # else NULL implicitly
+  files_in_dirs <- merge_url_lists(url_lists, directories, full_info)
+
+  #} # else NULL implicitly
 
   # Merge files at this level with files in subdirectories. Return the sorted
   # file list with attribute "failed" if any directory URL could not be accessed
@@ -103,143 +88,21 @@ list_url_ <- function(
   )
 }
 
-# response_to_data_frame -------------------------------------------------------
-response_to_data_frame <- function(response, full_info = FALSE)
+# mutate_or_not ----------------------------------------------------------------
+mutate_or_not <- function(x, prob = 0.1)
 {
-  template <- empty_file_info(full_info)
+  stopifnot(kwb.utils::inRange(prob, 0, 1))
 
-  # Response is NULL or equal to an empty string?
-  if (is.null(response) || grepl("^\\s*$", response)) {
-    return(template)
-  }
+  # Mutate with a probability of 10 %
+  if (prob > 0 && sample(c(TRUE, FALSE), 1, prob = c(prob, 1 - prob))) {
 
-  info <- response_to_data_frame_raw(response)
-
-  # Required column: is_directory
-  info$is_directory <- row_represents_directory(info)
-
-  if (! full_info) {
-    return(kwb.utils::selectColumns(info, c("file", "is_directory")))
-  }
-
-  #info$type <- ifelse(info$is_directory, "directory", "file")
-
-  # Replace columns "year_or_time", "month", "day" with "modification_time".
-  # Put the most important columns first.
-  main_columns_first(simplify_time_info(info))
-}
-
-# simplify_time_info -----------------------------------------------------------
-simplify_time_info <- function(info)
-{
-  # Add column "modification_time"
-  info$modification_time <- columns_to_timestamp(info)
-
-  # Remove the columns that were used to create the timestamps
-  kwb.utils::removeColumns(info, c("year_or_time", "month", "day"))
-}
-
-# columns_to_timestamp ---------------------------------------------------------
-columns_to_timestamp <- function(info)
-{
-  pull <- function(x) kwb.utils::selectColumns(info, x)
-
-  years_or_times <- pull("year_or_time")
-
-  is_year <- ! grepl(":", years_or_times)
-
-  # Compose a vector of timestamps. Use the current year in case of missing
-  # years, and use midnight in case of missing times
-  sprintf(
-    "%04d-%02d-%02d %s",
-    as.integer(ifelse(is_year, years_or_times, format(Sys.Date(), "%Y"))),
-    sapply(pull("month"), kwb.utils::selectElements, x = month_numbers()),
-    as.integer(pull("day")),
-    ifelse(is_year, "00:00", years_or_times)
-  )
-}
-
-# main_columns_first -----------------------------------------------------------
-main_columns_first <- function(df)
-{
-  columns <- intersect(names(empty_file_info(full_info = TRUE)), names(df))
-
-  kwb.utils::moveColumnsToFront(df, columns)
-}
-
-# response_to_data_frame_raw ---------------------------------------------------
-response_to_data_frame_raw <- function(response)
-{
-  # Split response at new line character into single rows
-  rows <- split_into_lines(response)
-
-  # Example row
-  # "-rw-r--r--    1 9261     15101        9132 Mar 27 15:05"
-
-  # Keep only the left-hand parts, representing eight columns (separated by one
-  # or more spaces), from each row
-  text <- kwb.utils::extractSubstring("^((([^ ]+) +){8})", rows, index = 1L)
-
-  # Convert the text rows into a data frame of file properties (except name)
-  info <- utils::read.table(
-    text = text, stringsAsFactors = FALSE, col.names = c(
-      "permissions", "links", "user", "group", "size", "month", "day",
-      "year_or_time"
-    )
-  )
-
-  # Set the filenames (right parts of the rows) in the returned info data frame
-  kwb.utils::setColumns(dbg = FALSE, info, file = mapply(
-    right, rows, nchar(rows) - nchar(text), USE.NAMES = FALSE
-  ))
-}
-
-# empty_file_info --------------------------------------------------------------
-empty_file_info <- function(full_info = TRUE)
-{
-  full_record <- data.frame(
-    file = character(),
-    is_directory = logical(),
-    #type = character(),
-    size = numeric(),
-    permissions = character(),
-    modification_time = character(),
-    user = character(),
-    group = character()
-  )
-
-  if (full_info) {
-
-    full_record
+    # Add some nonsense
+    paste0(x, "blabla")
 
   } else {
 
-    full_record[, 1:2]
+    x
   }
-}
-
-# set_file ---------------------------------------------------------------------
-set_file <- function(df, file)
-{
-  kwb.utils::setColumns(df, file = file, dbg = FALSE)
-}
-
-# get_file ---------------------------------------------------------------------
-get_file <- function(df)
-{
-  kwb.utils::selectColumns(df, "file")
-}
-
-# row_represents_directory -----------------------------------------------------
-row_represents_directory <- function(info)
-{
-  # Which files represent directories? If column "is_directory" is not yet
-  # available, look for a "d" at the beginning of the permission strings
-  # Extract permission strings (to check for the directory flag "d")
-  kwb.utils::defaultIfNULL(
-    info[["is_directory"]],
-    grepl("^d", kwb.utils::selectColumns(info, "permissions"))
-  )
 }
 
 # at_max_depth -----------------------------------------------------------------
@@ -261,13 +124,14 @@ merge_url_lists <- function(url_lists, directories, full_info)
   }
 
   # Merge the file lists returned for each directory
-  files <- kwb.utils::excludeNULL(
-    dbg = FALSE,
+  files <- silently_exclude_null(
     lapply(seq_along(url_lists), FUN = function(i) {
       #i <- 1
       urls <- url_lists[[i]]
       if (nrow(urls) > 0L) {
-        set_file(urls, paste0(directories[i], "/", get_file(urls)))
+        kwb.utils::setColumns(dbg = FALSE, urls, file = paste0(
+          directories[i], "/", kwb.utils::selectColumns(urls, "file")
+        ))
       } # else NULL implicitly
     })
   )
@@ -278,7 +142,13 @@ merge_url_lists <- function(url_lists, directories, full_info)
 
   # Return the vector of files with an attribute "failed" holding the merged
   # URLs of directories that could not be read
-  structure(bind_list_elements(files), failed = unlist(kwb.utils::excludeNULL(
-    lapply(url_lists, attr, "failed"), dbg = FALSE
-  )))
+  structure(bind_list_elements(files), failed = collect_failed(url_lists))
+}
+
+# collect_failed ---------------------------------------------------------------
+collect_failed <- function(x)
+{
+  stopifnot(is.list(x))
+
+  unlist(silently_exclude_null(lapply(x, attr, "failed")))
 }
