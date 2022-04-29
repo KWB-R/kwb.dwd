4e97460de978ad4f6208bbf0892978cb6c661263
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-04-29 02:44:03 +0200

    Add arguments "from", "to" to wrappers, document

--- R/load_monthly_berlin.R
+++ R/load_monthly_berlin.R
@@ -3,15 +3,17 @@
 #' This function loads monthly potential evaporation for Berlin, Germany,
 #' from Deutscher Wetterdienst (DWD).
 #'
+#' @param from optional. First month to be considered, as yyyymm string
+#' @param to optional. Last month to be considered, as yyyymm string
 #' @return data frame with columns \code{file} (name of file downloaded from
 #'   DWD), \code{year} (year number as integer), \code{month number as integer},
 #'   \code{mean} (mean value), \code{sd} (standard deviation), \code{min}
 #'   (minimum value), \code{max} (maximum value) of potential evaporation
 #'   calculated for Berlin, Germany
 #' @export
-load_potential_evaporation_berlin <- function(...)
+load_potential_evaporation_berlin <- function(from = NULL, to = NULL)
 {
-  load_monthly_variable_berlin(variable = "evapo_p", scale = 0.1, ...)
+  load_monthly_variable_berlin("evapo_p", scale = 0.1, from, to)
 }
 
 #' Load monthly precipitation for Berlin from DWD
@@ -19,15 +21,17 @@ load_potential_evaporation_berlin <- function(...)
 #' This function loads monthly precipitation for Berlin, Germany,
 #' from Deutscher Wetterdienst (DWD).
 #'
+#' @param from optional. First month to be considered, as yyyymm string
+#' @param to optional. Last month to be considered, as yyyymm string
 #' @return data frame with columns \code{file} (name of file downloaded from
 #'   DWD), \code{year} (year number as integer), \code{month number as integer},
 #'   \code{mean} (mean value), \code{sd} (standard deviation), \code{min}
 #'   (minimum value), \code{max} (maximum value) of precipitation
 #'   calculated for Berlin, Germany
 #' @export
-load_precipitation_berlin <- function(...)
+load_precipitation_berlin <- function(from = NULL, to = NULL)
 {
-  load_monthly_variable_berlin(variable = "precipitation", ...)
+  load_monthly_variable_berlin("precipitation", scale = NULL, from, to)
 }
 
 # load_monthly_variable_berlin -------------------------------------------------
