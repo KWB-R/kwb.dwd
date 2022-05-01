28fe74be995ec4b5d886bad0e778341b983eb90e
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-05-01 12:09:36 +0200

    Download projection file only once
    Store the file in "extdata/gk3.prj"

--- R/read_asc_gz_file.R
+++ R/read_asc_gz_file.R
@@ -45,7 +45,25 @@ provide_projection_file <- function(file)
     return()
   }
 
-  download.file(url_projection(), destfile, method = "auto")
+  prj_file <- file.path(system.file("extdata", package = "kwb.dwd"), "gk3.prj")
+
+  if (! file.exists(prj_file)) {
+    url <- url_projection()
+    kwb.utils::catAndRun(
+      sprintf(
+        "Downloading projection file from\n  %s\nto\n  %s",
+        url, prj_file
+      ),
+      newLine = 3L,
+      download.file(url, prj_file, method = "auto")
+    )
+  }
+
+  success <- file.copy(from = prj_file, to = destfile)
+
+  if (! all(success)) {
+    stop(sprintf("Could not copy %s to %s", prj_file, destfile))
+  }
 }
 
 # url_projection ---------------------------------------------------------------
