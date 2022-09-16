af391a8be168b2230efeb2ff204f878c4b7aae3f
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-09-16 12:59:07 +0200

    Move ftp_path*() to their own file

--- R/get_dwd_urls_metadata.R
+++ R/get_dwd_urls_metadata.R
@@ -122,24 +122,6 @@ dwd_url_climate_dir <- function(
   add_subdir(url, safe_element(currentness, c("historical", "recent")))
 }
 
-# ftp_path_cdc -----------------------------------------------------------------
-ftp_path_cdc <- function(...)
-{
-  file.path("ftp://opendata.dwd.de/climate_environment/CDC", ...)
-}
-
-# ftp_path_monthly_grids -------------------------------------------------------
-ftp_path_monthly_grids <- function(...)
-{
-  ftp_path_cdc("grids_germany/monthly", ...)
-}
-
-# ftp_path_daily_grids ---------------------------------------------------------
-ftp_path_daily_grids <- function(...)
-{
-  ftp_path_cdc("grids_germany/daily", ...)
-}
-
 # dwd_filename_stations --------------------------------------------------------
 dwd_filename_stations <- function(category, frequency)
 {
