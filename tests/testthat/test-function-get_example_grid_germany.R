77cae8abd797166368d0f4c45f7fef9f54461945
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-09-17 13:18:09 +0200

    Make all tests silent

--- tests/testthat/test-function-get_example_grid_germany.R
+++ tests/testthat/test-function-get_example_grid_germany.R
@@ -2,7 +2,7 @@ test_that("get_example_grid_germany() works", {
 
   f <- kwb.dwd:::get_example_grid_germany
 
-  capture.output(result <- f())
+  capture.output(suppressMessages(result <- f()))
 
   expect_true(inherits(result, "RasterLayer"))
 })
