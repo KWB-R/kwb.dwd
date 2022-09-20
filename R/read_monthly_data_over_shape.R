67a83b75fdf88b8ed1c397380bd2a3d331e424b6
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-09-20 18:01:14 +0200

    Allow to pass spatial object or no shape at all
    You may now read a shape file on your own,
    and pass the corresponding spatial object
    (or e.g. a subset of it) to the funcion
    read_monthly_data_over_shape(). You may also
    set both, "file" and "shape" to NULL and should
    receive data for the whole area of Germany.

--- R/read_monthly_data_over_shape.R
+++ R/read_monthly_data_over_shape.R
@@ -7,11 +7,24 @@
 #' @param from first month as "yyyymm" string
 #' @param to last month as "yyyymm" string
 #' @param quiet passed to \code{\link{download.file}}
+#' @param shape object of class sf or sf_layers or SpatialPolygonsDataFrame.
+#'   If \code{NULL} (the default), this object is obtained by calling
+#'   \code{kwb.dwd:::read_shape_file} on \code{file}.
+#' @param use_sf passed to \code{kwb.dwd:::read_shape_file} if applicable
 #' @param \dots further arguments passed to \code{kwb.dwd:::read_shape_file},
-#'   such as \code{drop_z = TRUE, use_sf = TRUE}
+#'   such as \code{drop_z = TRUE}, if applicable
 #' @return data frame
 #' @export
-read_monthly_data_over_shape <- function(file, variable, from, to, quiet = FALSE, ...)
+read_monthly_data_over_shape <- function(
+    file = NULL,
+    variable,
+    from,
+    to,
+    quiet = FALSE,
+    shape = NULL,
+    use_sf = TRUE,
+    ...
+  )
 {
   # Define scaling factors per variable. Depending on the variable, the values
   # in the data files need to be multiplied with a scaling factor.
@@ -22,17 +35,34 @@ read_monthly_data_over_shape <- function(file, variable, from, to, quiet = FALSE
   # Select the appropriate scaling factor
   scale <- kwb.utils::selectElements(scales, variable)
 
-  # Read shape file and transform to projection used in DWD's grid files
-  shape <- read_shape_with_dwd_projection(file, ...)
+  # If no spatial object is given in shape and if a shape file is given, read
+  # a spatial object from the shape file and transform it to the coordinate
+  # system used in the grids provided by DWD
+  if (is.null(shape) && !is.null(file)) {
+    shape <- read_shape_file(file, use_sf = use_sf, ...)
+  }
 
   # Download files from DWD server
-  grid_files <- download_monthly_grids_germany(variable, from, to, quiet = quiet)
+  grid_files <- download_monthly_grids_germany(
+    variable, from, to, quiet = quiet
+  )
 
   # Read the files into RasterLayer objects with DWD projection
   grids <- lapply(grid_files, read_asc_gz_file)
 
-  # Mask the full grid over Germany with the shape and crop the grid
-  grids <- mask_and_crop_grids(grids, shape)
+  # If a shape is given, mask the full grid over Germany with the shape and crop
+  # the grid
+  if (!is.null(shape) && length(grids)) {
+
+    # Transform the spatial object to the coordinate system used in the grids
+    shape <- transform_coords(
+      shape,
+      target_crs = grids[[1L]]@crs,
+      use_sf = use_sf
+    )
+
+    grids <- mask_and_crop_grids(grids, shape)
+  }
 
   # Calculate statistics, considering the conversion factor "scale"
   data <- do.call(rbind, lapply(grids, raster_stats, scale = scale))
