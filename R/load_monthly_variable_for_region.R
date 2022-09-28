6a10328e7f20bd1b25db71365cb59d32f045c4ed
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-09-28 07:56:17 +0200

    Simplify get_berlin_dwd_mask()
    use coordinate matrix directly within []

--- R/load_monthly_variable_for_region.R
+++ R/load_monthly_variable_for_region.R
@@ -65,19 +65,17 @@ load_monthly_variable_for_region <- function(
 #' get_berlin_dwd_mask()
 get_berlin_dwd_mask <- function()
 {
-  #DWD matrix filled with NA
+  # DWD matrix filled with NA
   berlin_matrix <- matrix(NA, nrow = 866, ncol = 654)
 
-  #get Berlin coordinates
-  berlin_coordinates <- utils::read.csv(system.file(
-    "extdata/berlin_coordinates.csv", package = "kwb.dwd"
-  ))
+  # file with coordinates of "Berlin cells" within DWD matrix
+  file <- system.file("extdata/berlin_coordinates.csv", package = "kwb.dwd")
 
-  #set Berlin cells to 1
+  # get Berlin coordinates
+  berlin_coordinates <- utils::read.csv(file)
 
-  for (i in seq_along(berlin_coordinates$row)) {
-    berlin_matrix[berlin_coordinates$row[i], berlin_coordinates$col[i]] <- 1
-  }
+  # set Berlin cells to 1
+  berlin_matrix[as.matrix(berlin_coordinates)] <- 1
 
   berlin_matrix
 }
