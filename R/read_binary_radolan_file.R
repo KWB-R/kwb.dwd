b5a38dd17a436b2b6f717b5c917d7c19f979936e
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-05-09 05:46:44 +0200

    Consider flags in read_binary_radolan_file()
    however not (yet) by default!

--- R/read_binary_radolan_file.R
+++ R/read_binary_radolan_file.R
@@ -7,13 +7,27 @@
 #'   and extent of the raster object are set to the Radolan specific values,
 #'   returned by \code{kwb.dwd::get_radolan_projection_string()} and
 #'   \code{kwb.dwd::get_radolan_extension()}, respectively.
+#' @param consider_flags logical. Should the flags be considered? If
+#'   \code{TRUE}, values where the "invalid" or "clutter" flag is set are set to
+#'   \code{NA} and values where the "negative" flag is set are negated. The
+#'   default is \code{TRUE} (for compatibility reasons) but this should change
+#'   in future!
 #' @export
 #' @importFrom sp CRS
 #' @importFrom raster extent
-read_binary_radolan_file <- function(path, set_projection_and_extent = TRUE)
+read_binary_radolan_file <- function(
+    path,
+    set_projection_and_extent = TRUE,
+    consider_flags = FALSE
+)
 {
   # Read raw data values
   rbi <- read_binary_radolan_file_raw(path)
+
+  # Consider flags, set e.g. invalid values to NA if requested
+  if (consider_flags) {
+    rbi <- consider_flags(rbi)
+  }
 
   # Convert raw data values to raster
   rb <- radolan_raw_to_raster(rbi)
@@ -40,4 +54,20 @@ read_binary_radolan_file <- function(path, set_projection_and_extent = TRUE)
 get_radolan_extension <- function()
 {
   c(-523.4622, 376.5378, -4658.645, -3758.645)
+}
+
+# consider_flags ---------------------------------------------------------------
+#' @importFrom kwb.utils getAttribute
+consider_flags <- function(x, flags = kwb.utils::getAttribute(x, "flags"))
+{
+  bit_is_set <- function(x, bit) bitwAnd(x, 2^(bit - 1L)) > 0L
+
+  is_invalid <- bit_is_set(flags, 14L)
+  is_negative <- bit_is_set(flags, 15L)
+  is_clutter <- bit_is_set(flags, 16L)
+
+  x[is_negative] <- -x[is_negative]
+  x[is_invalid | is_clutter] <- NA
+
+  x
 }
