cd8a974b9a1e1b05db896a2ddf55092a7cd247d1
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-05-19 13:00:44 +0200

    Adapt/fix/improve tests

--- tests/testthat/test-function-download_daily_grids_germany.R
+++ tests/testthat/test-function-download_daily_grids_germany.R
@@ -1,9 +1,11 @@
+#library(testthat)
+
 test_that("download_daily_grids_germany() works", {
 
   f <- kwb.dwd:::download_daily_grids_germany
 
   expect_error(f())
-  expect_error(f("x"))
+  expect_error(f("x"), "must be one of 'evapo_p'")
 
   capture.output(result <- f(variable = "evapo_p", quiet = TRUE))
 
