a8c607b0a5ee373a19c138289265d66a237932e9
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-05-22 15:48:41 +0200

    Consider locally available shape file in test

--- tests/testthat/test-function-check_or_download_shapes_germany.R
+++ tests/testthat/test-function-check_or_download_shapes_germany.R
@@ -4,8 +4,21 @@ test_that("check_or_download_shapes_germany() works", {
 
   f <- kwb.dwd:::check_or_download_shapes_germany
 
-  expect_error(
-    shape_file <- f(quiet = TRUE, timeout = 1),
-    "Could not download .* within 1 seconds"
-  )
+  result <- try(f(quiet = FALSE, timeout = 1))
+
+  if (kwb.utils::isTryError(result)) {
+
+    expect_match(
+      as.character(result),
+      "Could not download .* within 1 seconds"
+    )
+
+
+  } else {
+
+    expect_true(file.exists(result))
+    #unlink(result, recursive = TRUE)
+    #unlink("~/../Downloads/shapes_germany/gadm40_DEU_shp.zip")
+  }
+
 })
