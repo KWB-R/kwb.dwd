fde728b87863b1fb4a6184b511616eda9d8841bb
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-05-25 11:36:35 +0200

    Set "use_sf" to TRUE if x inherits from "sf"

--- R/transform_coords.R
+++ R/transform_coords.R
@@ -1,5 +1,5 @@
 # transform_coords -------------------------------------------------------------
-transform_coords <- function(x, target_crs = NULL, use_sf = FALSE)
+transform_coords <- function(x, target_crs = NULL, use_sf = inherits(x, "sf"))
 {
   if (is.null(target_crs)) {
     return(x)
